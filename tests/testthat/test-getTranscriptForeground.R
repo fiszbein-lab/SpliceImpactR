e00dd6819b3c7eddacd2493aa46ab7b4c99a3a26
zachpwakefield <zachpwakefield@gmail.com>
2025-10-31 12:57:27 -0400

    removed old tests

--- tests/testthat/test-getTranscriptForeground.R
+++
@@ -1,3 +0,0 @@
-test_that("getTranscriptForeground works", {
-  expect_equal(2 * 2, 4)
-})
--- tests/testthat/test-getTranslations.R
+++
@@ -1,9 +0,0 @@
-test_that("getTranslations works with sample data", {
-  pdir <- system.file("extdata", package = "SpliceImpactR")
-  dataDirectory <- paste0(pdir, "/")
-
-  translations <- getTranslations(translations_location = dataDirectory)
-
-  expect_type(translations, "character")
-  expect_gt(length(translations), 0)
-})
